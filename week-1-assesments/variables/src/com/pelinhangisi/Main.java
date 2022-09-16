479586b03859b98538d0d1ccff93d5fc0c94eb07
Pelin Hangişi <pelinhangisi@gmail.com>
2022-09-16 12:27:43 +0300

    variables done

---
+++ week-1-assesments/variables/src/com/pelinhangisi/Main.java
@@ -0,0 +1,19 @@
+package com.pelinhangisi;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Öğrenci sayım : 10");
+        System.out.println("Öğrenci sayım : 10");
+        System.out.println("Öğrenci sayım : 10");
+        System.out.println("Öğrenci sayım : 10");
+
+        int ogrenciSayisi = 12;
+        String mesaj="Öğrenci sayım : ";
+
+        System.out.println("Öğrenci sayım : " + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+    }
+}
