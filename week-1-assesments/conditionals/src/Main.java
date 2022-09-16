226b00d841f4a6fb6c2822fb6908d25480f65464
Pelin Hangişi <pelinhangisi@gmail.com>
2022-09-16 12:59:00 +0300

    conditionals done.

---
+++ week-1-assesments/conditionals/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+	    int sayi = 24;
+
+        if(sayi<20){
+            System.out.println("Sayı 20'den küçüktür.");
+        }else if (sayi==20){
+            System.out.println("Sayı 20'ye eşittir.");
+        }else {
+            System.out.println("Sayı 20'den büyüktür.");
+        }
+    }
+}
