ee6da4dde325602d9bcb26c69daacef6bb04214b
Pelin Hangişi <pelinhangisi@gmail.com>
2022-09-16 12:49:33 +0300

    dataTypes done.

---
+++ week-1-assesments/dataTypes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        double sayi = 12.5;
+        sayi = -129;
+
+        String sehir = "Kocaeli";
+        char karakter = 'A';
+
+        boolean dogruMu = false;
+
+    }
+}
