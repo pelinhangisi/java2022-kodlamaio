6549f021850266e1d08089d3bfea6a6c3161303e
Pelin Hangişi <pelinhangisi@gmail.com>
2022-09-21 22:12:31 +0300

    day2 first part

--- oop1/src/Main.java
+++ oop1/src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
+        String mesaj = "Vade oranı";
 
+        Product product1 = new Product();
+                        // set value
+        product1.name ="Delonghi Kahve Makinesi";
+        product1.unitPrice = 7500;
+        product1.discount = 7;
+        product1.unitsInStock = 3;
+        product1.imageUrl = "bilmemne.jpg";
+
+                        //get
+        System.out.println(product1.name);
     }
 }
---
+++ oop1/src/Product.java
@@ -0,0 +1,11 @@
+
+
+//PascalCase
+public class Product {
+    String name;
+    //camelCase
+    double unitPrice;
+    double discount;
+    String imageUrl;
+    int unitsInStock;
+}
