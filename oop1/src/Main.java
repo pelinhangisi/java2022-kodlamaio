9ce8de3ca22dfa376fce5a904bc7420a99731f61
Pelin Hangişi <pelinhangisi@gmail.com>
2022-09-21 23:00:08 +0300

    day 2- part 2

--- oop1/src/Main.java
+++ oop1/src/Main.java
@@ -4,14 +4,31 @@ public class Main {
         String mesaj = "Vade oranı";
 
         Product product1 = new Product();
-                        // set value
-        product1.name ="Delonghi Kahve Makinesi";
-        product1.unitPrice = 7500;
-        product1.discount = 7;
-        product1.unitsInStock = 3;
-        product1.imageUrl = "bilmemne.jpg";
+        product1.setName("Delonghi Kahve Makinesi");
+        product1.setDiscount(7);
+        product1.setUnitPrice(7500);
+        product1.setUnitsInStock(3);
+        product1.setImageUrl("image1.jpg");
 
-                        //get
-        System.out.println(product1.name);
+        Product product2 = new Product();
+        product2.setName("Smeg Kahve Makinesi");
+        product2.setDiscount(7);
+        product2.setUnitPrice(7500);
+        product2.setUnitsInStock(3);
+        product2.setImageUrl("image2.jpg");
+
+        Product product3 = new Product();
+        product3.setName("Kitchen Aid Kahve Makinesi");
+        product3.setDiscount(7);
+        product3.setUnitPrice(7500);
+        product3.setUnitsInStock(3);
+        product3.setImageUrl("image3.jpg");
+
+        System.out.println("<ul>");
+        Product[] products = {product1, product2, product3};
+        for(Product product : products){
+            System.out.println("<li>"+ product.getName()+ "</li>");
+        }
+        System.out.println("</ul>");
     }
 }
--- oop1/src/Product.java
+++ oop1/src/Product.java
@@ -2,10 +2,51 @@
 
 //PascalCase
 public class Product {
-    String name;
+    private String name;
     //camelCase
-    double unitPrice;
-    double discount;
-    String imageUrl;
-    int unitsInStock;
+    private double unitPrice;
+    private double discount;
+    private String imageUrl;
+    private int unitsInStock;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getDiscount() {
+        discount = discount + 5;
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(int unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
 }
