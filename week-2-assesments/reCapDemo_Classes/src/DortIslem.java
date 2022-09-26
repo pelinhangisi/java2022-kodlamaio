ab64e4ca136e03585599214cc86e0dbfce5ccc5d
Pelin Hangişi <pelinhangisi@gmail.com>
2022-09-26 13:31:42 +0300

    reCapDemo_Classes done.

---
+++ week-2-assesments/reCapDemo_Classes/src/DortIslem.java
@@ -0,0 +1,18 @@
+public class DortIslem {
+
+    public int Topla(int sayi1, int sayi2){
+        return sayi1+sayi2;
+    }
+
+    public int Cikar(int sayi1, int sayi2){
+        return sayi1-sayi2;
+    }
+
+    public int Carp(int sayi1, int sayi2){
+        return sayi1*sayi2;
+    }
+
+    public int Bol(int sayi1, int sayi2){
+        return sayi1/sayi2;
+    }
+}
---
+++ week-2-assesments/reCapDemo_Classes/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.Topla(3,4);
+        System.out.println(sonuc);
+    }
+}
