08aa057cb65d38db32dd3236ce07ffb8469e974a
Pelin Hangişi <pelinhangisi@gmail.com>
2022-09-26 11:37:17 +0300

    sayiBulma done.

---
+++ week-2-assesments/sayiBulma/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] sayilar = new int[]{1,2,5,7,9,0};
+        int aranacak = 8;
+        boolean varMi = false;
+
+        for(int sayi : sayilar){
+            if(sayi == aranacak){
+                varMi = true;
+                break;
+            }
+        }
+        if(varMi){
+            System.out.println("Sayı mevcuttur.");
+        }else {
+            System.out.println("Sayı mevcut değildir.");
+        }
+    }
+}
