b66ad0038013546cd021a809fb0403196dc3cf0c
Pelin Hangişi <pelinhangisi@gmail.com>
2022-09-26 14:38:32 +0300

    methodOverloading done.

---
+++ week-2-assesments/methodOverloading/src/DortIslem.java
@@ -0,0 +1,10 @@
+public class DortIslem {
+
+    public int topla(int sayi1, int sayi2){
+        return sayi1+sayi2;
+    }
+
+    public int topla(int sayi1, int sayi2, int sayi3){
+        return sayi1+sayi2;
+    }
+}
---
+++ week-2-assesments/methodOverloading/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+	    DortIslem dortIslem = new DortIslem();
+        System.out.println(dortIslem.topla(2,3));
+        dortIslem.topla(2,3,5);
+    }
+}
