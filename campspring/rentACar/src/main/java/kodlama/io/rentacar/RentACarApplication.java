43b0f9d06a7d0e4bb2862ccc80d96de36b591525
Pelin Hangişi <pelinhangisi@gmail.com>
2022-10-25 21:49:13 +0300

    maven project created- first part

---
+++ campspring/rentACar/src/main/java/kodlama/io/rentacar/RentACarApplication.java
@@ -0,0 +1,13 @@
+package kodlama.io.rentacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentACarApplication.class, args);
+    }
+
+}
---
+++ campspring/rentACar/src/test/java/kodlama/io/rentacar/RentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package kodlama.io.rentacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
