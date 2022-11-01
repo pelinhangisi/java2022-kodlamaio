3d20ce93c96c5b8e7179940e85c608b3dac6d616
Pelin Hangişi <pelinhangisi@gmail.com>
2022-11-01 16:09:48 +0300

    kodlama.io.Devs project file created

---
+++ week-4-assesments/kodlama-io-Devs/src/main/java/kodlama/io/devs/KodlamaIoDevsApplication.java
@@ -0,0 +1,13 @@
+package kodlama.io.devs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodlamaIoDevsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodlamaIoDevsApplication.class, args);
+    }
+
+}
---
+++ week-4-assesments/kodlama-io-Devs/src/test/java/kodlama/io/devs/KodlamaIoDevsApplicationTests.java
@@ -0,0 +1,13 @@
+package kodlama.io.devs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodlamaIoDevsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
